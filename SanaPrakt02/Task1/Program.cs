45627f4224e7103738bbe729416a28ccba63c6e2
Bohdan Serdeniuk <sana22_sbo@student.ztu.edu.ua>
2022-11-11 17:38:45 +0200

    point 20

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -2,6 +2,5 @@
 Console.WriteLine("text1");
 Console.WriteLine("text2");
 Console.WriteLine("text3");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("text4");
+Console.WriteLine("text5");
